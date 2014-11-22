b1c90765fa50fca0ebc0972fd67349007ccb6a39
Andrew Babkin <ab@izh.com>
2014-11-22 20:00:54 +0400

    some fixes and write documentation

--- lib/class.htmlparse.php
+++ lib/class.htmlparse.php
@@ -6,28 +6,52 @@ class HTMLParse {
     const HTTP_EQUIV = 'http-equiv';
     const CONTENT_TYPE = 'content-type';
     const CONTENT = 'content';
+    const CHARSET = 'charset';
+
+    const ERROR_PAGE = '/_probably_undefined_page';
 
     private $text;
     private $dom;
 
-    public function __construct ($url) {
+    private function request ($url) {
+        $ch = curl_init();
 
-        $ch = curl_init($url);
-
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_ENCODING, '');
+ 
+        $text = curl_exec($ch);
 
-        $this->text = curl_exec($ch);
+        $this->info = $info = curl_getinfo($ch);
 
-        if ($this->text) {
+        $code = $info['http_code'];
+        if ($code >= 300 && $code < 400) {
+
+            return $this->request($info['redirect_url']);
+
+        } elseif ($code < 400 && $text) {
             $this->dom = new \DOMDocument();
             libxml_use_internal_errors(TRUE);
-            $this->dom->loadHTML($this->text);
+            $this->dom->loadHTML($text);
             libxml_clear_errors();
             libxml_use_internal_errors(FALSE);
+            
+            return $text;            
         }
+
+    }
+
+    private function extractCharset ($content) {
+        if (preg_match('~charset=([^;]+)~i', $content, $matches)) {
+            return strtolower(trim($matches[1]));    
+        }
+    }
+
+    public function __construct ($url) {
+        $this->url = $url;
+        $this->text = $this->request($url);
     }
 
     public function getText () {
@@ -35,28 +59,38 @@ class HTMLParse {
     }
 
     public function getCharset () {
-        $list = $this->dom->getElementsByTagName('meta');
         $result = NULL;
 
-        foreach ($list as $item) {
-            
-            $attrs = array();
-            foreach ($item->attributes as $attr) {
-                $attrs[strtolower($attr->name)] = $attr->value;
-            }
+        if (array_key_exists('content_type', $this->info)) {
+            $result = $this->extractCharset($this->info['content_type']);    
+        }
 
-            if (array_key_exists(self::HTTP_EQUIV, $attrs) &&
-                strtolower($attrs[self::HTTP_EQUIV]) === self::CONTENT_TYPE) {
+        if (!$result && $this->dom) {
+            $list = $this->dom->getElementsByTagName('meta');
+
+            foreach ($list as $item) {
                 
-                $content = (array_key_exists(self::CONTENT, $attrs)) 
-                        ? $attrs[self::CONTENT] : '';
+                // collectattributes on each meta tag
+                $attrs = array();
+                foreach ($item->attributes as $attr) {
+                    $attrs[strtolower($attr->name)] = $attr->value;
+                }
 
-                if (preg_match('~charset=([^;]+)~i', $content, $matches)) {
-                    $result = strtolower(trim($matches[1]));    
-                    break;
+                if (array_key_exists(self::CHARSET, $attrs)) {
+
+                    $result = $attrs[self::CHARSET];
+
+                } elseif (array_key_exists(self::HTTP_EQUIV, $attrs) &&
+                    strtolower($attrs[self::HTTP_EQUIV]) === self::CONTENT_TYPE) {
+
+                    $content = (array_key_exists(self::CONTENT, $attrs)) 
+                            ? $attrs[self::CONTENT] : '';
+                    $result = $this->extractCharset($content);
+                    if (boolval($result)) {
+                        break;
+                    }
                 }
             }
-
         }
 
         return $result;
--- test/sample.php
+++ test/sample.php
@@ -3,13 +3,15 @@
 require_once dirname( __DIR__ ) . '/lib/class.htmlparse.php';
 
 $url = 'http://habrahabr.ru';
-$url = 'https://client.fieldforce.ru/login';
+$url = 'https://client.fieldforce.ru';
 $url = 'http://www.yandex.ru';
 //$url = 'http://www.ya.ru';
 //$url = 'http://www.ucoz.ru';
 //$url = 'http://lib.ru';
+//$url = 'http://taobao.com';
 
 $site = new Yaseek\HTMLParse($url);
+//var_dump($site);
 
 file_put_contents(__DIR__ . '/site.html', $site->getText());
 
